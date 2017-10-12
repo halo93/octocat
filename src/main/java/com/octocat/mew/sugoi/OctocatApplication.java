42b6378c92185dc723b7c0e3dc6538f2d9632340
Do Ha Long <dohalong1993@gmail.com>
2017-10-12 16:03:33 +0700

    Add application config + modify main class

--- src/main/java/com/octocat/mew/sugoi/OctocatApplication.java
+++ src/main/java/com/octocat/mew/sugoi/OctocatApplication.java
@@ -3,7 +3,7 @@ package com.octocat.mew.sugoi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.octocat.mew.sugoi"})
 public class OctocatApplication {
 
 	public static void main(String[] args) {
