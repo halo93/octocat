ee46c31c0391979d66f8b2a6080424495ae19380
dohalong <dohalong@framgia0198-pc.framgia.com>
2017-10-12 15:04:53 +0700

    Initial commit

---
+++ src/main/java/com/octocat/mew/sugoi/OctocatApplication.java
@@ -0,0 +1,12 @@
+package com.octocat.mew.sugoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctocatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctocatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/octocat/mew/sugoi/OctocatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.octocat.mew.sugoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OctocatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
